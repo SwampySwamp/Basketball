04e98ded7cda4b68428ba7d61ed19f386b7b6d57
DESKTOP-B5T5HOP\SuperGPU <raoul.bickmann@googlemail.com>
2017-04-25 15:53:06 +0200

    COmmite Projekt full

---
+++ VRTK-master/Assets/Basketball/Scripts/recall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recall : MonoBehaviour {
+
+	public GameObject Player;
+	public bool clicked = false;
+	private Vector offset;
+
+	// Use this for initialization
+	void Start () {
+		clicked = GetComponent<VRTK_ControllerEvents> ().GripPressed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		clicked = GetComponent<VRTK_ControllerEvents>().gripPressed;
+
+		if (clicked) {
+			SetPositionAndRotation (offset, 0);
+		}
+			
+	}
+}
